186a489e57f581498a4e541cf08feee6aad15204
Vy <motchiecvee@Little-Vee.local>
2023-05-07 00:47:06 +0700

    create handleLogout function

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,85 +1,92 @@
-import React from 'react'
-import { NavLink, Link } from 'react-router-dom';
-import styles from './header.module.scss';
-
+import React from "react";
+import { useDispatch } from "react-redux";
+import { NavLink, Link } from "react-router-dom";
+import { logout } from "../../Redux/slices/userSlice";
+import styles from "./header.module.scss";
 
 function Header() {
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(logout());
+    localStorage.removeItem("accessToken");
+  };
+
   return (
     <header className={`${styles.header} fixed-top`}>
-      <div className={styles.header__nav} id='nav-test'>
-        <nav className='navbar navbar-expand-lg navbar-light bg-light text-align-item-center'>
-          <Link to='/' className={`${styles.header__logo} ml-3 mt-2 mb-0`}>
-            <img src='../image/logo-main.png' alt='logoMain' className='mb-0' />
+      <div className={styles.header__nav} id="nav-test">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light text-align-item-center">
+          <Link to="/" className={`${styles.header__logo} ml-3 mt-2 mb-0`}>
+            <img src="../image/logo-main.png" alt="logoMain" className="mb-0" />
           </Link>
           <button
-            className='navbar-toggler'
-            type='button'
-            data-toggle='collapse'
-            data-target='#navbarSupportedContent'
-            aria-controls='navbarSupportedContent'
-            aria-expanded='false'
-            aria-label='Toggle navigation'
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
           >
-            <span className='navbar-toggler-icon' />
+            <span className="navbar-toggler-icon" />
           </button>
 
           <div
             className={` ${styles.header__middle} collapse navbar-collapse`}
-            id='navbarSupportedContent'
+            id="navbarSupportedContent"
           >
-            <ul className='navbar-nav mt-0 ml-auto'>
-              <li className='nav-item '>
+            <ul className="navbar-nav mt-0 ml-auto">
+              <li className="nav-item ">
                 <Link
-                  className='nav-link text-left text-md-center'
-                  to='#'
-                  href='#'
-                // onClick={() => {
-                //   document
-                //     .getElementById('lichChieu')
-                //     .scrollIntoView({ behavior: 'smooth' });
-                // }}
+                  className="nav-link text-left text-md-center"
+                  to="#"
+                  href="#"
+                  // onClick={() => {
+                  //   document
+                  //     .getElementById('lichChieu')
+                  //     .scrollIntoView({ behavior: 'smooth' });
+                  // }}
                 >
                   Lịch chiếu
                 </Link>
               </li>
-              <li className='nav-item  '>
+              <li className="nav-item  ">
                 <Link
-                  className='nav-link text-left text-md-center'
-                  to='#'
-                  href='#'
-                // onClick={() => {
-                //   document
-                //     .getElementById('cumRap')
-                //     .scrollIntoView({ behavior: 'smooth' });
-                // }}
+                  className="nav-link text-left text-md-center"
+                  to="#"
+                  href="#"
+                  // onClick={() => {
+                  //   document
+                  //     .getElementById('cumRap')
+                  //     .scrollIntoView({ behavior: 'smooth' });
+                  // }}
                 >
-                  Cụm rạp{' '}
+                  Cụm rạp{" "}
                 </Link>
               </li>
-              <li className='nav-item '>
+              <li className="nav-item ">
                 <Link
-                  className='nav-link text-left text-md-center'
-                  to='#'
-                  href='#'
-                // onClick={() => {
-                //   document
-                //     .getElementById('tinTuc')
-                //     .scrollIntoView({ behavior: 'smooth' });
-                // }}
+                  className="nav-link text-left text-md-center"
+                  to="#"
+                  href="#"
+                  // onClick={() => {
+                  //   document
+                  //     .getElementById('tinTuc')
+                  //     .scrollIntoView({ behavior: 'smooth' });
+                  // }}
                 >
-                  Tin tức{' '}
+                  Tin tức{" "}
                 </Link>
               </li>
-              <li className='nav-item '>
+              <li className="nav-item ">
                 <Link
-                  className='nav-link text-left text-md-center'
-                  to='#'
-                  href='#'
-                // onClick={() => {
-                //   document
-                //     .getElementById('ungDung')
-                //     .scrollIntoView({ behavior: 'smooth' });
-                // }}
+                  className="nav-link text-left text-md-center"
+                  to="#"
+                  href="#"
+                  // onClick={() => {
+                  //   document
+                  //     .getElementById('ungDung')
+                  //     .scrollIntoView({ behavior: 'smooth' });
+                  // }}
                 >
                   Ứng dụng
                 </Link>
@@ -107,19 +114,19 @@ function Header() {
             </ul>
           </div>
 
-          <div className=''>
-              <button
-                className='btn logout'
-                // onClick={handleLogOut}
-                style={{ textAlign: 'left' }}
-              >
-                Đăng Xuất
-              </button>{' '}
+          <div className="">
+            <button
+              className="btn logout"
+              // onClick={handleLogOut}
+              style={{ textAlign: "left" }}
+            >
+              Đăng Xuất
+            </button>{" "}
           </div>
         </nav>
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
